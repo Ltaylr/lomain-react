a54d423f86cdc5d19d8748e1f2f60ba41f501725
Ltaylor <ltaylr@protonmail.com>
2025-08-29 12:52:50 -0400

    working on sidebar css

--- src/views/Navigation.tsx
+++ src/views/Navigation.tsx
@@ -7,10 +7,10 @@ const Navigation = () => {
   return (
     <>
       <nav className="flex flex-col justify-evenly gap-10  pl-2">
-        <div><NavLink className="mr-3 md:mr-10 border p-2 block hover:text-shadow hover:text-shadow-pink-500 hover:text-shadow-lg font-extrabold text-green-400 hover:text-3xl md:text-2xl text-lg" to="/">H o m e</NavLink></div>
-        <div><NavLink className="mr-3 md:mr-10 border p-2 block hover:text-shadow hover:text-shadow-pink-500 hover:text-shadow-lg font-extrabold text-green-400 hover:text-3xl md:text-2xl text-lg" to="/About">A b o u t</NavLink></div>
-        <div><NavLink className="mr-3 md:mr-10 border p-2 block hover:text-shadow hover:text-shadow-pink-500 hover:text-shadow-lg font-extrabold text-green-400 hover:text-3xl md:text-2xl text-lg" to="/Characters">C h a r a c t e r s</NavLink></div>
-        <div><NavLink className="mr-3 md:mr-10 border p-2 block hover:text-shadow hover:text-shadow-pink-500 hover:text-shadow-lg font-extrabold text-green-400 hover:text-3xl md:text-2xl text-lg" to="/Campaigns">C a m p a i g n s</NavLink></div>
+        <div className="md:flex text-center"><img className="w-[50%] md:w-[10%] md:mr-5 md:ml-5 md:mb-0 mb-3" src='/limeD20.svg'/><NavLink className="mr-3 md:mr-10 border p-2 block hover:text-shadow hover:text-shadow-pink-500 hover:text-shadow-lg font-extrabold text-green-400 md:text-2xl text-md" to="/">H o m e</NavLink></div>
+        <div className="md:flex text-center"><img className="w-[50%] md:w-[10%] md:mr-5 md:ml-5 md:mb-0 mb-3" src='/limeD20.svg'/><NavLink className="mr-3 md:mr-10 border p-2 block hover:text-shadow hover:text-shadow-pink-500 hover:text-shadow-lg font-extrabold text-green-400 md:text-2xl text-md" to="/About">A b o u t</NavLink></div>
+        <div className="md:flex text-center"><img className="w-[50%] md:w-[10%] md:mr-5 md:ml-5 md:mb-0 mb-3" src='/limeD20.svg'/><NavLink className="mr-3 md:mr-10 border p-2 block hover:text-shadow hover:text-shadow-pink-500 hover:text-shadow-lg font-extrabold text-green-400 md:text-2xl text-md" to="/Characters">C h a r a c t e r s</NavLink></div>
+        <div className="md:flex text-center"><img className="w-[50%] md:w-[10%] md:mr-5 md:ml-5 md:mb-0 mb-3" src='/limeD20.svg'/><NavLink className="mr-3 md:mr-10 border p-2 block hover:text-shadow hover:text-shadow-pink-500 hover:text-shadow-lg font-extrabold text-green-400 md:text-2xl text-md" to="/Campaigns">C a m p a i g n s</NavLink></div>
       </nav>
       
     </>
